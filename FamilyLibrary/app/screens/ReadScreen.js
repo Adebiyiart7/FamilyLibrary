a63edc7b3523e5679524fd8c064ab624d483c59b
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-02-21 20:35:47 +0100

    Working on read screen

--- FamilyLibrary/app/screens/ReadScreen.js
+++ FamilyLibrary/app/screens/ReadScreen.js
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useSelector } from "react-redux";
+import Markdown from "react-native-markdown-display";
 
-const ReadScreen = () => {
+// LOCAL IMPORTS
+import defaultStyles from "../config/styles";
+import Screen from "../components/Screen";
+import AppHeader from "../components/AppHeader";
+import AppText from "../components/AppText";
+
+const ReadScreen = ({ route }) => {
+  const { books } = useSelector((state) => state.books);
+  const book = books.find((b) => b._id === route.params._id);
+
   return (
-    <View>
-      <Text>ReadScreen</Text>
-    </View>
-  )
-}
+    <Screen header={<AppHeader title={book.title} />}>
+      <AppText style={styles.fullText}>
+        <Markdown>{book.fullText}</Markdown>
+      </AppText>
+    </Screen>
+  );
+};
 
-export default ReadScreen
+export default ReadScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  fullText: defaultStyles.readingText
+});
