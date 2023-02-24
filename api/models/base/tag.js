dfbdc77c40f23041c54d65595dd1c7c8f53dfbc3
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-02-24 15:18:59 +0100

    Updated the tags model

--- api/models/base/tag.js
+++ api/models/base/tag.js
@@ -1,21 +1,27 @@
 const mongoose = require("mongoose");
 
-const tagSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    max: 255,
-    lowercase: true,
+const tagSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      max: 255,
+      lowercase: true
+    },
+    numberOfBooks: {
+      type: Number,
+      default: 0
+    },
+    followersCount: {
+      type: Number,
+      default: 0
+    }
   },
-  numberOfBooks: {
-    type: Number,
-    default: 0,
-  },
-}, {
-  timestamps: true,
-}
-)
+  {
+    timestamps: true
+  }
+);
 
 module.exports = mongoose.model("Tag", tagSchema);
