ed26f72560d128886e2a8e5beaea580b5977e10d
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-02-24 08:13:10 +0100

    Paginated get 20 tags per page

---
+++ api/controllers/base/tag/get20Tags.js
@@ -0,0 +1,16 @@
+const Tag = require("../../../models/base/tag");
+const { apiResponse } = require("../../../utils");
+
+/**
+ * @route       POST /api/base/tag/get20Tags
+ * @desc        get 20 tag; paginated in 20s
+ * @access      public
+ */
+const get20Tags = async (req, res) => {
+  const tags = await Tag.find().select("-__v");
+  return res
+    .status(201)
+    .json(apiResponse(res.statusCode, "Request successful", tags));
+};
+
+module.exports = get20Tags;
