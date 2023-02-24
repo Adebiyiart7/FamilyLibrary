9552d96a871af4495fb8a2622cdf17569d6b905c
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-02-24 15:16:04 +0100

    Created a base data feature for data that needs to be loaded immediately the app loads

---
+++ FamilyLibrary/app/features/base/baseSlice.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+// LOCAL IMPORTS
+import { API_URI, axiosConfig } from "../config";
+
+const initialState = {
+  tags: [],
+  isLoading: false,
+  isSuccess: false,
+  isError: false,
+  message: ""
+};
+
+export const getTags = createAsyncThunk("base/tags", async (thunkAPI) => {
+  try {
+    const response = await axios.get(API_URI + "/base/tags/get20tags");
+
+    if (response.data) {
+      return response.data.body;
+    }
+  } catch (error) {
+    const message = error.response.data.body.message.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
+const baseSlice = createSlice({
+  name: "base",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.isError = false;
+      state.message = "";
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getTags.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getTags.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.tags = action.payload;
+      })
+      .addCase(getTags.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  }
+});
+
+export default baseSlice.reducer;
+export const { reset } = baseSlice.actions;
